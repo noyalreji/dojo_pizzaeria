fc024b34ad24971b17db98833adb2d32e09b5367
Noyal Jacob <johndoe@example.comNoyal>
2021-07-02 11:37:57 -0500

    using variables and arrays

---
+++ pizza.js
@@ -0,0 +1,40 @@
+function pizzaOven(crustType, sauceType, cheeses, toppings) {
+  var pizza = {};
+  pizza.crustType = crustType;
+  pizza.sauceType = sauceType;
+  pizza.cheeses = cheeses;
+  pizza.toppings = toppings;
+  return pizza;
+}
+console.log(pizzaOven("deep dish", "traditional", ["mozzarella"], ["pepperoni", "sausage"]));
+
+function pizzaOven(crustType, sauceType, cheeses, toppings) {
+  var pizza = {};
+  pizza.crustType = crustType;
+  pizza.sauceType = sauceType;
+  pizza.cheeses = cheeses;
+  pizza.toppings = toppings;
+  return pizza;
+}
+console.log(pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]));
+
+function pizzaOven(crustType, sauceType, cheeses, toppings) {
+  var pizza = {};
+  pizza.crustType = crustType;
+  pizza.sauceType = sauceType;
+  pizza.cheeses = cheeses;
+  pizza.toppings = toppings;
+  return pizza;
+}
+console.log(pizzaOven("hand tossed", "traditional", ["cheddar"], ["halapeneos", "chicken"]));
+
+function pizzaOven(crustType, sauceType, cheeses, toppings) {
+  var pizza = {};
+  pizza.crustType = crustType;
+  pizza.sauceType = sauceType;
+  pizza.cheeses = cheeses;
+  pizza.toppings = toppings;
+  return pizza;
+}
+console.log(pizzaOven("thin-curst", "traditional", ["cheddar"], ["halapeneos", "beef"]));
+
